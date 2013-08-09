5504b0dbd329776b74b221cfc3cb30075d489cae
heysion <heysions@gmail.com>
2013-08-10 00:34:14 +0800

    syslog test

---
+++ 2013/exercise/809/wsyslog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+
+#define LOG_NAME "TestLog"
+
+int main(int argc,char **argv)
+{
+  openlog(LOG_NAME,LOG_CONS|LOG_PID,LOG_USER);
+
+  if( argc != 2)
+    {
+      syslog(LOG_INFO,"error in %s\n",argv[0]);
+      closelog();
+      return 0;
+    }
+
+  syslog(LOG_DEBUG,"log in %s \n message: %s\n",argv[0],argv[1]);
+  closelog();
+  return 0;
+
+}
+
