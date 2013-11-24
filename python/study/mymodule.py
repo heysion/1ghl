54e593dad82dd0954fa2b9cb9ff18173f6e9b2a2
heysion <heysions@gmail.com>
2013-11-24 16:39:34 +0800

    update python learn -- module

---
+++ python/study/mymodule.py
@@ -0,0 +1,7 @@
+#this is my first python module
+
+def my_func():
+    print 'this is my module'
+
+the_version = '0.1'
+
---
+++ python/study/use-mymodule.py
@@ -0,0 +1,7 @@
+#use my first module
+
+import mymodule 
+
+mymodule.my_func()
+
+print mymodule.the_version
