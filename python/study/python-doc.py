50f3c62f6369b74a2e1935268800e20b94513740
heysion <heysions@gmail.com>
2013-11-24 16:21:06 +0800

    update python learn

---
+++ python/study/python-doc.py
@@ -0,0 +1,13 @@
+#func doc
+
+def func():
+    '''abc test
+    just test '''
+    x = 10
+    print x
+
+func()
+
+print func()
+
+print func.__doc__
---
+++ python/study/return-argv.py
@@ -0,0 +1,11 @@
+#return value
+
+def fun(a=None) :
+    if a != None:
+        return a
+    else:
+        return -1
+
+print fun()
+
+print fun(1)
