e19e4650020e543925c6f3865ec16b9f30398330
heysion <heysions@gmail.com>
2013-11-27 22:03:33 +0800

    update 11-27-13

---
+++ python/study/use_class.py
@@ -0,0 +1,21 @@
+#use class
+class people:
+    name = ''
+    age = 0
+    __weight = 0
+
+    def __init__(self,n,a,w):
+        self.name = n 
+        self.age = a 
+        self.__weight = w
+
+    def p(self):
+        print('the name is %s,%d old ' %(self.name,self.age))
+
+
+
+p = people('name',10,20)
+p.p()
+
+people.p()
+
