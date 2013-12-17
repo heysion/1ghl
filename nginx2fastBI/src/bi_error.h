775403679f9500dcc245db2fbe14594cf711fc5b
heysion <heysions@gmail.com>
2013-12-17 10:43:27 +0800

    update 12-17-13

---
+++ nginx2fastBI/src/bi_error.h
@@ -0,0 +1,12 @@
+/* 
+ *fastBI error file
+ */
+#ifndef _BI_ERROR_H_
+#define _BI_ERROR_H_
+
+typedef int             bi_err_t
+#define BI_EPERM        EPERM
+#define BI_ENOENT       ENOENT
+
+
+#endif
