39e907cb699aed0ddeb8a37acac3a4670d8d21ec
heysion <heysions@gmail.com>
2013-12-27 16:51:41 +0800

    python qt center

--- python/study/gui/test.py
+++ python/study/gui/test.py
@@ -8,6 +8,10 @@ def accept2():
 	print 'hell'
 def reject2():
 	print 'hell2'
+def clicked1():
+	print 'hell3'
+def clicked2():
+	print 'hell4'
 
 
 class MainWindow(QMainWindow, Ui_Dialog):  
@@ -17,10 +21,17 @@ class MainWindow(QMainWindow, Ui_Dialog):
 #            self.buttonBox.connect(self.accepted)
 	    QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), accept2)
 	    QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), reject2)
-
+	    QtCore.QObject.connect(self.checkBox, QtCore.SIGNAL("clicked()"), clicked1)
+	    QtCore.QObject.connect(self.btn_OK, QtCore.SIGNAL("clicked()"), clicked2)
+#	    self.center()
 if __name__ == '__main__':  
     app = QApplication(sys.argv)  
     mainWnd = MainWindow()  
     mainWnd.show()  
+#    width_len = QApplication.desktop().width()
+    width_len = (QApplication.desktop().width() - mainWnd.width() ) / 2
+    height_len = (QApplication.desktop().height() - mainWnd.height() ) / 2
+    print width_len , height_len
+    mainWnd.move(width_len,height_len)
     app.exec_()  
 
--- python/study/gui/testgui.py
+++ python/study/gui/testgui.py
@@ -2,34 +2,34 @@
 
 # Form implementation generated from reading ui file 'untitled.ui'
 #
-# Created: Sat Dec 21 20:38:05 2013
+# Created: Fri Dec 27 16:07:16 2013
 #      by: pyside-uic 0.2.13 running on PySide 1.1.0
 #
 # WARNING! All changes made in this file will be lost!
 
 from PySide import QtCore, QtGui
 
-
-
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
+        Dialog.resize(415, 233)
         self.buttonBox = QtGui.QDialogButtonBox(Dialog)
-        self.buttonBox.setGeometry(QtCore.QRect(30, 240, 341, 32))
+        self.buttonBox.setGeometry(QtCore.QRect(20, 170, 341, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
         self.checkBox = QtGui.QCheckBox(Dialog)
-        self.checkBox.setGeometry(QtCore.QRect(40, 140, 104, 28))
+        self.checkBox.setGeometry(QtCore.QRect(40, 110, 104, 28))
         self.checkBox.setObjectName("checkBox")
+        self.btn_OK = QtGui.QPushButton(Dialog)
+        self.btn_OK.setGeometry(QtCore.QRect(240, 100, 111, 33))
+        self.btn_OK.setObjectName("btn_OK")
 
         self.retranslateUi(Dialog)
-#        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), Dialog.accept)
-#        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), Dialog.reject)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
         self.checkBox.setText(QtGui.QApplication.translate("Dialog", "加密", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_OK.setText(QtGui.QApplication.translate("Dialog", "测试", None, QtGui.QApplication.UnicodeUTF8))
 
