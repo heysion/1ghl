34fdfda23267f3810527d77854cc7b7d4182fbe7
heysion <heysions@gmail.com>
2014-01-08 17:05:19 +0800

    test in c language

---
+++ 2014/use-c-language/static_file.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "t_static.h"
+
+int main()
+{
+
+  pf();
+  printf("%d\n",T_b);
+  return 0;
+}
---
+++ 2014/use-c-language/t_static.h
@@ -0,0 +1,11 @@
+#ifndef _T_STATIC_
+#define _T_STATIC_
+#include <stdio.h>
+
+static int T_b=100;
+static void pf()
+{
+  printf("PF\n");
+}
+
+#endif
