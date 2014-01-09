9941a20031a9e9fa3f44ecea5fc3ab2a2020bd09
heysion <heysions@gmail.com>
2014-01-09 15:11:24 +0800

    新文件：    2014/use-c-language/mem_list.c

---
+++ 2014/use-c-language/mem_list.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "memwatch.h"
+
+typedef struct mem_list * plist_t ;
+typedef struct mem_list list_t ;
+typedef struct mem_list
+{
+  void *d;
+  list_t *next;
+}mlist;
+
+int add_list(list_t *pool,void *d)
+{
+  list_t *t;
+  list_t *temp;
+ 
+  if(NULL != pool)
+    {
+      t = pool->next;
+      if(NULL == t && NULL == pool->d)
+	{
+	  pool->d = d ;
+	  pool->next = NULL ;
+	  return 0;
+	}else if(NULL == t && NULL != pool->d)
+	{
+	  temp = (list_t *)malloc(sizeof(list_t)) ;
+	  temp->d = (void *)d ;
+	  temp->next = NULL ;
+	  pool->next = temp ;
+
+	  return 0;
+	}
+      else
+	{
+	  temp = (list_t *)malloc(sizeof(list_t)) ;
+	  temp->d = d ;
+	  temp->next = pool->next ;
+	  pool->next= temp ;
+
+	  return 0;
+	}
+    }
+  else
+    {
+      return -1;
+    }
+  return 1;
+}
+
+int del_list(list_t *pool)
+{
+  int i=0;
+  list_t *t;
+  void * temp;
+
+  /*
+  for(t=pool;t!=NULL;t=t->next)
+    {
+      temp = t->d;
+      free(temp);
+      free(t);      
+      i++;
+      printf("del :%d \n",i);
+
+    }
+  */
+  t = pool->next ;
+  free(pool->d);
+  free(pool);
+  pool= t;
+  t = pool->next ;
+  free(pool->d);
+  free(pool);
+  pool= t;
+  t = pool->next ;
+  free(pool->d);
+  free(pool);
+  
+  return 0;
+}
+int main()
+{
+  list_t *pool ;
+  char *p[3];
+  int i =0;
+
+  mwDoFlush(1);
+  pool = (plist_t )malloc(sizeof(list_t)) ;
+  pool->next = NULL ;
+  pool->d = NULL ;
+  for(;i<3;i++)
+    {
+      p[i]=(char *)malloc(i+10);
+      add_list(pool,(void *)p[i]);
+    }
+  
+  del_list(pool);
+  return 0;
+}
