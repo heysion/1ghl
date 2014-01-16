089757f93e59f70a2224982ba6274932e9df2bbe
heysion <heysions@gmail.com>
2014-01-16 22:17:40 +0800

    update 16-01-2014

--- 2014/use-c-language/p_type.c
+++ 2014/use-c-language/p_type.c
@@ -21,18 +21,18 @@ int main()
 
   for(i=0;i<8;i++)
     {
-      printf("#%d,%p\n",sizeof(pa[i]),pa[i].p);
+      printf("#%d,%p\n",sizeof(pa[i].p),pa[i].p);
       pa[i].p=(char *)malloc((i+1)*2);
 
     }
   pp = &pa ;
   
 
-  //  printf("@%d,%p\n",sizeof(p),p[1]);
+  printf("@%d,%p\n",sizeof(pp),pp[1]);
   //  printf("@%d,%d\n",sizeof(p),sizeof(p[2]));
   for(i=0;i<8;i++)
     {
-      printf("%d,%p\n",sizeof(pa[i]),pa[i].p);
+
       free(pa[i].p);
       pa[i].p=NULL;
 
