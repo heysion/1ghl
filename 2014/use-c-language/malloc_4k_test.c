f0f0bd7fd9179e641b8267367caf6293fd8d9953
heysion <heysions@gmail.com>
2014-01-25 16:30:15 +0800

    zoom loop number

--- 2014/use-c-language/malloc_4k_test.c
+++ 2014/use-c-language/malloc_4k_test.c
@@ -16,49 +16,53 @@ int main(int argc ,char **argv)
 
 
   void *p ;
-  int i = 0;
-  int n = 0 ,t = 0;
+  int i = 0,k =0 ;
+  int n = 0 ,t = 0,t2=0;
 
-  if(argc != 2)
+  if(argc != 3)
     {
       printf("argv error %ds",argc);
       return 0;
     }
   t = atoi(argv[1]);
+  t2 = atoi(argv[2]);
   n = t >10 ? t : 10 ;
 
   p = malloc(10);
   free(p);
   p =NULL ;
   time_init(star,end,diff);
-  printf("%d ,%d ,%d\n",star.tv_usec,end.tv_usec,diff.tv_usec);
+  printf("%d : %d ,%d ,%d\n",n,star.tv_usec,end.tv_usec,diff.tv_usec);
 
   time_star(star) ;
-
-  for(i=0 ;i < n ;i++)
+  for (k=0;k<t2;k++)
     {
-      p =malloc(4096);
-      free(p);
-      p = NULL ;
+      for(i=0 ;i < n ;i++)
+	{
+	  p =malloc(4096);
+	  free(p);
+	  p = NULL ;
+	}
     }
-
   time_stop(end);
   time_diff(star,end,diff);
 
   printf("%d : %d ,%d ,%d\n",n,star.tv_usec,end.tv_usec,diff.tv_usec);
 
-  n = (t == n ) ? n * 10 : t * 10 ; 
+  //  n = (t == n ) ? n * 10 : t * 10 ; 
   time_reset(star,end,diff);
+  printf("%d : %d ,%d ,%d\n",n,star.tv_usec,end.tv_usec,diff.tv_usec);
 
   time_star(star) ;
-
-  for(i=0 ;i < n ; i++)
+  for (k=0;k<t2;k++)
     {
-      p =malloc(409);
-      free(p);
-      p = NULL ;
+      for(i=0 ;i < n ; i++)
+	{
+	  p =malloc(4096);
+	  free(p);
+	  p = NULL ;
+	}
     }
-
   time_stop(end);
   time_diff(star,end,diff);
   printf("%d : %d ,%d ,%d\n",n,star.tv_usec,end.tv_usec,diff.tv_usec);
